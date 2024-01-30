6e4fd2c45322a57531280723fb8c032faba7cb28
Judith Vanessa Mallama <judith.mallama@udea.edu.co>
2024-01-30 18:06:16 -0500

    Ejercicio 1 terminado, el ejercicio es de una lista de tareas, que tiene diferentes fucionalidades

--- Ejercicio-condicionales/EjerciciosJavaScript/Ejer1-ListadeTareas.js
+++ Ejercicio-condicionales/EjerciciosJavaScript/Ejer1-ListadeTareas.js
@@ -1,97 +1,111 @@
 alert("Lista de tareas")
 
 // Lista de tareas:
-// Solicitar al usuario que ingrese una lista de tareas que deba realizar, esta lista se
-// debe guardar en un colección de objetos, donde cada objeto representa una tarea.
-// Cada tarea debe tener la siguiente información: nombre o descripción de la tarea y
-// estado de la tarea (completada o pendiente).
+// Solicitar al usuario que ingrese una lista de tareas que deba realizar, esta lista se*
+// debe guardar en un colección de objetos, donde cada objeto representa una tarea.*
+// Cada tarea debe tener la siguiente información: nombre o descripción de la tarea y*
+// estado de la tarea (completada o pendiente).*
 // Se le solicita que cree y ejecute con datos de prueba las siguientes funciones:
-// Una función que guarde un listado de tarea
+// Una función que guarde un listado de tarea 
 // Una función que cambie el estado de una tarea
 // Una función que filtre las tareas por estado (completadas y pendientes).
-// Una función que agregue una tarea al listado
+// Una función que agregue una tarea al listado*
 // Una función que elimine una tarea.
 // Consúltele al usuario qué acción desea realizar en el programa haciendo uso de
 // una estructura switch case.
 
+//********************************* */
 
-/*Lista de tareas*/
-const listaDeTareas = []
+// Lista de tareas:
+ const taskList = []
 
-//Declaración de la función agregar tarea
-const agregarTarea = (nombre, estado) => {
-    const tarea ={
-        nombre,
-        estado,
+ //Una función que agregue una tarea al listado
+ const addTask = (name, description, state) => {
+    //debe guardar en un colección de objetos, donde cada objeto representa una tarea.
+    // Cada tarea debe tener la siguiente información: nombre o descripción de la tarea y
+    // estado de la tarea (completada o pendiente).
+    const task = {
+        name,
+        description,
+        state,
     }
-    listaDeTareas.push(tarea)
-    console.log("Lista de tareas actualizadas", listaDeTareas)
+    taskList.push(task)
+    console.log("Lista de tareas actualizada",taskList)
+ }
+
+ //Función cambiar estdo de una tarea por nombre
+const changueStatusName = (name, state) => {
+    const task = taskList.find(item => item.name === name)
+    task.state = state
+    console.log("Lista de tareas actualizada",taskList)
 }
 
-//Declaración de la función cambiar estado por el nombre de la tarea
-const cambiarEstadoPorNombre = (nombre, estado) =>{
-    const tarea = listaDeTareas.find(item => item.nombre === nombre)
-    tarea.estado = estado
-    console.log("tarea", tarea)
-    console.log("Lista de tareas actualizadas", listaDeTareas)
+ //Función cambiar estdo de una tarea por posicion
+ const changueStatusPosicion = (posicion, state) => {
+    taskList[posicion].state = state
+    console.log("Lista de tareas actualizada",taskList)
+ }
+ 
+//Una función que filtre las tareas por estado (completadas y pendientes).
+const filterTask = (state) => {
+    const filteredTask = taskList.filter(item => item.state === state)
+    console.log("Lista de tareas actualizada", filteredTask)
 }
 
-//Declaración de la fucon cabiar estado por posisción
-const cambiarEstadoPorPosicion = (posicion, estado) =>{
-     listaDeTareas[posicion].estado=estado
-     console.log("Lista de tareas actualizadas", listaDeTareas)
+//Función eliminar tarea por posisicon
+const deleteTaskPosicion = (posicion) =>{
+    taskList.splice(posicion,1)
 }
 
-//Declaración de la función de filtro tarea por estado 
-const filtrarTareas = (estado) => {
-    const tareasFiltradas = listaDeTareas.filter(item => item.estado === estado)
-    console.log("Lista de tareas actualizadas", tareasFiltradas)
+//Función eliminar la ultima tarea ingresada
+const deleteLastTask = (posicion) =>{
+    taskList.pop()
+}
+ // Solicitar al usuario que ingrese una lista de tareas que deba realizar
+const homework = prompt("Ingrese la tarea")
+const descripcionTask = prompt("Ingrese la descripción de la tarea")
+const stateTask = prompt("Ingrese el estado de la tarea")
+addTask(homework, descripcionTask, stateTask) 
+let newTask = prompt("Desea ingresar una nueva tarea?")
+
+while(newTask === 'si'){
+    const homework = prompt("Ingrese la tarea")
+    const descripcionTask = prompt("Ingrese la descripción de la tarea")
+    const stateTask = prompt("Ingrese el estado de la tarea")
+    addTask(homework, descripcionTask, stateTask) 
+    newTask = prompt("Desea ingresar una nueva tarea?")
+    console.log(newTask);
 }
 
-//Declaraci{on de la funcioon para eliminar tareas
-const eliminarTareasPorPosicion = (posicion) => {
-    listaDeTareas.splice(posicion,1)
-}
 
-//Declaraci{on de la funcion para eliminar la ultima tarea
-const eliminarUltimaTaea = () => {
-    listaDeTareas.pop()
-}
+// Consúltele al usuario qué acción desea realizar en el programa haciendo uso de
+// una estructura switch case.
+let  functionExecute = prompt("Por favor digite la función que desea ejecutar")
 
-
-const nombreTarea = prompt("Por favor ingrese el nombre de la tarea")
-const estadoTarea = prompt("Por favor ingrese el estado de la tarea")
-agregarTarea(nombreTarea,estadoTarea)
-let seguirEjecutando =prompt("Desea sguir igresando tareas?")
-
-while(seguirEjecutando  === 'si'){
-    const nombreTarea = prompt("Por favor ingrese el nombre de la tarea")
-    const estadoTarea = prompt("Por favor ingrese el estado de la tarea")
-    agregarTarea(nombreTarea,estadoTarea)
-    seguirEjecutando =prompt("Desea sguir igresando tareas?")
-}
-
-const funcionEjecutar = prompt("Por favor digite el nombre de la funcion")
-
-switch (funcionEjecutar) {
-    case 'agregarTarea':
-        agregarTarea("Tarea generica", "completar")
+while(functionExecute !== null){
+    switch(functionExecute){
+        case '1':
+            addTask("Tarea generica", "Cualquier cosa", "completada")
         break;
-    case 'cambiarEstadoPorNombre':
-        cambiarEstadoPorNombre("tarea generica", "pendiente")
+        case '2':
+            changueStatusName("Tarea generica","pendiente")
         break;
-    case 'cambiarEstadoPorPosicion':
-        cambiarEstadoPorPosicion(1, "completada")
+        case '3':
+            changueStatusPosicion(1, "completada")
         break;
-    case 'filtrarTareas':
-        filtrarTareas("pendiente")
+        case '4':
+            filterTask("pendiente")
         break;
-    case  'eliminarTareasPorPosicion':
-        eliminarTareasPorPosicion(1)
+        case '5':
+            deleteTaskPosicion(1)
         break;
-    case 'eliminarUltimaTaea':
-        eliminarUltimaTaea()
+        case '6':
+            deleteLastTask()
         break;
-    default:
-        break;
+        default:
+            break
+    }
+    functionExecute = prompt("Por favor digite la función que desea ejecutar")
 }
+
+
