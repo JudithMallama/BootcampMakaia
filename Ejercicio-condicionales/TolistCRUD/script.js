99c8b873c30297baecf94aceeb330936c12a9c7d
Judith Vanessa Mallama <judith.mallama@udea.edu.co>
2024-02-04 22:47:27 -0500

    Ejerccio del dia 29 de enero del 2024 lunes, formulario de tienda guardar y agregar

---
+++ Ejercicio-condicionales/TolistCRUD/script.js
@@ -0,0 +1,101 @@
+/**
+ * Crear un do list y realizar su respectivo  CRUD create Read Update Delete, filtro por estado
+ */
+
+alert("Ejercicio de list")
+
+// array de tareas
+const listaDeTareas = []
+const tareasFiltradas = []
+
+const editarEstado = (e, indice, estado) => {
+    e.preventDefault()
+    console.log("evento on change", e)
+    const newEstado = e.target.value
+    cambiarEstadoPorPosicion(indice, newEstado)
+    listarTareas()
+}
+
+const editarTarea = (e, indice, estado) => {
+    e.preventDefault()
+    console.log("estado", estado);
+    const span = document.getElementById(`tarea${indice}`)
+    span.innerHTML = `
+    <select name="estado" onchange= "editarEstado(event, ${indice}, '${estado}')">
+    <option value="pendiente" ${estado=='pendiente' ? 'selected' : null}>Pendiente</option>
+    <option value="ejecutando" ${estado=='ejecutando' ? 'selected' : null} >Ejecutando</option>
+    <option value="finalizado"  ${estado=='finalizado' ? 'selected' : null}>Finalizado</option>
+     </select>`
+}
+
+const listarTareas = () => {
+    const section = document.getElementById("listaTareas")
+    let htmlLista = ''
+
+    listaDeTareas.forEach(
+        (tarea, index) => (htmlLista +=
+            `<div class="listaItem">
+            <span>${tarea.nombre}</span>
+            <span id="tarea${index}">${tarea.estado}</span>
+            <button type"button" onclick="editarTarea(event, ${index}, '${tarea.estado}')">Editar</button>
+            <button type="button" onclick="eliminarTareaPorPosicion(${index})">Eliminar</button>
+        </div>`))
+    section.innerHTML = htmlLista
+}
+
+const recibirForm = (e) => {
+    e.preventDefault()
+    const form = document.getElementById("agregarTarea")
+    const formData = new FormData(form)
+    const json = {}
+
+    for ([key, value] of formData.entries()) {
+        json[key] = value
+    }
+
+    form.reset()
+    agregarTarea(json.tarea, json.estado)
+    console.log("Lista de tareas", listaDeTareas);
+}
+
+// declaracion de la funcion agregar tarea
+const agregarTarea = (nombre, estado) => {
+    const tarea = {
+        nombre,
+        estado,
+    }
+    listaDeTareas.push(tarea)
+    listarTareas()
+}
+// declaracion de la funcion cambiar estado por el nombre de la tarea
+const cambiarEstadoPorNombre = (nombre, estado) => {
+    const tarea = listaDeTareas.find(item => item.nombre === nombre)
+    tarea.estado = estado
+    console.log("Lista de tareas actualizada", listaDeTareas)
+}
+
+// declaracion de la funcion cambiar estado por posicion
+const cambiarEstadoPorPosicion = (posicion, estado) => {
+    listaDeTareas[posicion].estado = estado
+    console.log("Lista de tareas actualizada", listaDeTareas)
+}
+
+//declaracion de la funcion de filtrar tareas por estado
+const filtrarTareas = (estado) => {
+    const tareasFiltradas = listaDeTareas.filter(item => item.estado === estado)
+    return tareasFiltradas
+}
+
+//declaracion de la funcion para eliminar tareas
+const eliminarTareaPorPosicion = (posicion) => {
+    listaDeTareas.splice(posicion, 1)
+    listarTareas()
+}
+
+//declaracion de la funcion para eliminar la ultima tarea
+const eliminarUltimaTarea = () => {
+    listaDeTareas.pop()
+}
+
+listarTareas()
+
--- EjerciciosFormularios/script.js
+++ EjerciciosFormularios/script.js
@@ -1,12 +1,12 @@
 const products = [
     {
-        id:1,
+        id: 1,
         nombre: "Camiseta Hombre",
-        categoria:['Masculino' , 'Camisetas'],
-        imagenes:["https://th.bing.com/th/id/OIP.pliP-_RtQrIm5ftUwU21XgHaHr?rs=1&pid=ImgDetMain", "https://pettegole.com/shop/9761-medium_default/camiseta-2523.jpg", "https://estampadoypublicidad.com/wp-content/uploads/2018/10/Camisetas-personalizadas-10.jpg"
+        categoria: ['Masculino', 'Camisetas'],
+        imagenes: ["https://th.bing.com/th/id/OIP.pliP-_RtQrIm5ftUwU21XgHaHr?rs=1&pid=ImgDetMain", "https://pettegole.com/shop/9761-medium_default/camiseta-2523.jpg", "https://estampadoypublicidad.com/wp-content/uploads/2018/10/Camisetas-personalizadas-10.jpg"
         ],
         precioUnitario: 50000,
-        stock:[
+        stock: [
             {
                 talla: "S",
                 cantidad: 20,
@@ -31,13 +31,13 @@ const products = [
     },
 
     {
-        id:2,
+        id: 2,
         nombre: "Vestido Dama",
-        categoria:['Masculino' , 'Camisetas'],
-        imagenes:["https://i.pinimg.com/originals/0a/3b/7e/0a3b7eb60cad26b3a536d51af138d94f.jpg", "https://bridalroomboutique.com/wp-content/uploads/2021/05/mejores-precios-vestidos-fiesta-eventos-celebraciones-prom-isla-margarita-venezuela-evening-dress-boutique-caracas.jpg", "https://cdn0.bodas.com.mx/cat/vestidos-fiesta/pronovias/atos-style-16--mfvo278249.jpg"
+        categoria: ['Masculino', 'Camisetas'],
+        imagenes: ["https://i.pinimg.com/originals/0a/3b/7e/0a3b7eb60cad26b3a536d51af138d94f.jpg", "https://bridalroomboutique.com/wp-content/uploads/2021/05/mejores-precios-vestidos-fiesta-eventos-celebraciones-prom-isla-margarita-venezuela-evening-dress-boutique-caracas.jpg", "https://cdn0.bodas.com.mx/cat/vestidos-fiesta/pronovias/atos-style-16--mfvo278249.jpg"
         ],
         precioUnitario: 100000,
-        stock:[
+        stock: [
             {
                 talla: "S",
                 cantidad: 2,
@@ -75,22 +75,75 @@ const insertarProductos = (contenedor, listaProductos) => {
             <figure>
                 <img src= ${producto.imagenes[0]} alt=${producto.nombre} >
             </figure>
-            <span>$ ${producto.precioUnitario.toLocaleString()}</span>
+            <span>$ ${producto.precioUnitario.toLocaleString()} </span>
             <h3><p>${producto.nombre}<p></h3>
         </article>
         `
     });
 }
+
+const obtenerDatosDelForm = (form) => {
+    const formData = new FormData(form)
+    const dataForm = {}
+    for (const [key, value] of formData.entries()) {
+        dataForm[key] = value
+    }
+    return dataForm
+}
+
+
+const agragarProducto = (dataForm, listaProductos) => {
+
+    const productoExixtente = listaProductos.find(
+        (producto) => producto.nombre === dataForm.nombre
+    )
+    if (productoExixtente) {
+        const stock = productoExixtente.stock.find(producto => producto.talla === dataForm.talla && producto.color === dataForm.color)
+
+        if (stock) {
+            stock.cantidad += dataForm.cantidad
+            //Actualizacion de imagenes
+            productoExixtente.imagenes = [dataForm.imagen1, dataForm.imagen2, dataForm.imagen3]
+        } else {
+            productoExixtente.imagenes.push(
+                dataForm.imagen1, 
+                dataForm.imagen2, 
+                dataForm.imagen3, 
+            )
+            stock.push({
+                talla: dataForm.talla,
+                cantidad: dataForm.cantidad,
+                color: dataForm.color
+            })
+        }
+    } else {
+        const productoNuevo = {
+            id: listaProductos.length + 1,
+            nombre: dataForm.nombre,
+            categoria: [dataForm.genero, dataForm.tipo],
+            imagenes: [dataForm.imagen1, dataForm.imagen2, dataForm.imagen3],
+            precioUnitario: dataForm.precio,
+            stock: [
+                {
+                    talla: dataForm.talla,
+                    cantidad: dataForm.cantidad,
+                    color: dataForm.color
+                }
+            ]
+        }
+        listaProductos.push(productoNuevo)
+    }
+
+}
+
+
 insertarProductos(contenedorProductos, products)
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
-   const formData = new FormData(form)
-   const dataForm ={}
-   
-   console.log(formData.entries());
-   for(const [key, value] of formData.entries()){
-        dataForm[key]=value;
-   }
-   console.log(dataForm);
+
+    const newProduct = obtenerDatosDelForm(form)
+    agragarProducto(newProduct, products)
+    insertarProductos(contenedorProductos, products)
+    console.log(products);
 })
